59f9fce6b3af086ce2e1859b99df6501f60722e0
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-03-03 20:21:02 +0700

    first commit

---
+++ LuasLingkaran.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//deklarasi variable
+float jarijari;
+float phi = 3.14159;
+//fungsi awal
+ void awal(){
+    cout << "Masukkan jari-jari lingkaran: ";
+    cin >> jarijari;
+ }
+ float luas (float jari_jari, float phi){
+    return phi * jari_jari * jari_jari;
+ }
+ void akhir (){
+    cout << "Hasil luas untuk lingkaran dengan jari jari" << jarijari<< luas (jarijari,phi);
+ }
+ int main (){
+    awal();
+    akhir ();
+ }
